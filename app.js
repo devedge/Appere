6d31d8605cf959d942ee2293b04f4ab5664fdc15
devedge <devedge@outlook.com>
2016-09-24 22:23:55 -0400

    Fine tuned sorting algorithm

--- app.js
+++ app.js
@@ -11,36 +11,29 @@ const key_right = 39;
 const key_esc = 27;
 const key_del = 46;
 
-// Image directory info and lists
-// var img_dir_list = [{}, {}, {}];
-
-// Template for an image directory
-// var img_dir = {
-//     'abs_path': '',
-//     'filename_array': []
-// }
-
-// var img_list = fs_manager.gen_img_list();
-
-// var wraparound = false;
-
 // HTML element that contains the image
 var img_element = document.getElementById('image-container');
 
+// Disallows the next image to be requested before the directory scanner is done
+// var ready = false;
 
 
 // todo: check that a file exists using fs.existsSync
 // todo: on every call, update the image list array
 // Set the current image from a filepath
 function setCurrentImage(filepath) {
-    try {
-        img_element.src = path.join(path.dirname(filepath), pEncode(path.basename(filepath)));
+    var dn = path.dirname(filepath);
+    var bn = path.basename(filepath);
 
-        mngr.genList(path.dirname(filepath), path.basename(filepath), (err) => {
-            if (err) {
-                console.log(err);
-            }
-        });
+    try {
+        if (mngr.checkFile(bn)) {
+            img_element.src = path.join(dn, pEncode(bn));
+            document.title = 'Appere — ' + bn;
+
+            mngr.genList(dn, bn, (err) => {
+                if (err) { console.log(err); }
+            });
+        }
 
     } catch (e) {
         // log error, but need to call an error function instead
@@ -48,31 +41,65 @@ function setCurrentImage(filepath) {
     }
 }
 
+
 // Set the previous image in the image list
 function setPreviousImage() {
-    img_element.src = path.join(mngr.getCurrentDir(), pEncode(mngr.getPrev(true)));
+    mngr.getPrev(true, (prev_ready, fp) => {
+
+        // If the image list has been generated, then this function call is ready
+        if (prev_ready) {
+            document.title = 'Appere — ' + fp;
+            img_element.src = path.join(mngr.getCurrentDir(), pEncode(fp));
+            // if (img_element.classList.contains('scale-full')) {
+                img_element.classList.add('scale-fit');
+                img_element.classList.remove('scale-full');
+            // }
+        }
+    });
 }
+
 
 // Set the next image in the image list
 function setNextImage() {
-    img_element.src = path.join(mngr.getCurrentDir(), pEncode(mngr.getNext(true)));
+    mngr.getNext(true, (next_ready, fp) => {
+
+        // If the image list has been generated, then this function call is ready
+        if (next_ready) {
+            document.title = 'Appere — ' + fp;
+            img_element.src = path.join(mngr.getCurrentDir(), pEncode(fp));
+            // if (img_element.classList.contains('scale-full')) {
+                img_element.classList.add('scale-fit');
+                img_element.classList.remove('scale-full');
+            // }
+        }
+    });
 }
 
+// function displayError() {
+//
+// }
 
 
 // Keylistener logic
 document.addEventListener('keydown', function(event) {
+    console.log(event.keyCode);
 
     // Read the keycode property of the key pressed
     switch (event.keyCode) {
         case key_left: {
             event.preventDefault();
+
+            console.log('Left');
             setPreviousImage();
+
             break;
         }
         case key_right: {
             event.preventDefault();
+
+            console.log('Right');
             setNextImage();
+
             break;
         }
         case key_esc: {
@@ -91,32 +118,56 @@ document.addEventListener('keydown', function(event) {
 
             break;
         }
+        case 32: {
+            event.preventDefault();
+
+            img_element.classList.toggle('scale-fit');
+            img_element.classList.toggle('scale-full');
+
+            // img_element.className = img_element.className.replace('set-fit', 'set-full');
+
+            break;
+        }
         // default: {}
             // do nothing, ignore the key
     }
 });
 
-var called = false;
+
 
 // Handle any images drag-and-dropped onto the display window
 // disable default 'onDrop' event
-document.ondragover = document.ondrop = (event) => {
+// document.ondragover = document.ondrop = (event) => {
+//     event.preventDefault();
+//
+//     if (!called) {
+//         setCurrentImage(event.dataTransfer.files[0].path);
+//
+//         console.log('2: ' + event.dataTransfer.files[0].path);
+//
+//         called = true;
+//     }
+// }
+
+// Prevent anything from happening when an item is only dragged over
+document.ondragover = (event) => {
     event.preventDefault();
-
-    if (!called) {
-        setCurrentImage(event.dataTransfer.files[0].path);
-
-        console.log('2: ' + event.dataTransfer.files[0].path);
-        
-        called = true;
-    }
-
 }
 
-// display the image into the viewer
-document.body.ondrop = (event) => {
-    setCurrentImage(event.dataTransfer.files[0].path);
+// Display the image in the viewer
+// Handle onDrop event
+document.ondrop = (event) => {
     event.preventDefault();
+    setCurrentImage(event.dataTransfer.files[0].path);
+
+    // (temporary) log the image path
+    console.log('2: ' + event.dataTransfer.files[0].path);
+}
+
+// Handle body.onDrop event
+document.body.ondrop = (event) => {
+    event.preventDefault();
+    setCurrentImage(event.dataTransfer.files[0].path);
 
     // (temporary) log the image path
     console.log('1: ' + event.dataTransfer.files[0].path);
--- js/FSManager.js
+++ js/FSManager.js
@@ -5,50 +5,121 @@ const frep = require('frep');
 // The supported image filetypes
 const supported_types = /^(\.jpg|\.jpeg|\.jpe|\.jfif|\.jif|\.gif|\.png|\.bmp|\.svg|\.ico)$/;
 
+// Flag that indicates if the image list is loaded
+var list_loaded = false;
+
 
 // FileSystem manager constructor. Handles generating an array of every supported
 // filetype in a folder, and functions to retrieve them.
 function FSmanager() {
     this.folder = '';
     this.img_list = [];
-    this.current_index = 0;
     this.extension = '';
+    this.current_index = 0;
 }
 
 
 // Generate an array list of filenames that can be called
 // to set the next image
+// todo: put a hard limit to the size of the number of files?
 FSmanager.prototype.genList = function(filepath, current_filename, cb) {
 
-    // prevent regenerating the image list for the same filepath
+    // Don't regenerate the image list for the same filepath
     if (filepath !== this.folder) {
+        // Set the 'list_loaded' flag to false. This prevents the 'getPrev',
+        // 'getNext', (etc) function from getting called before the image array
+        // is generated.
+        list_loaded = false;
 
-        console.log('genList run');
-
+        // Reset the image list to an empty array.
+        // (to optimize, save the last two arrays?)
+        this.img_list = [];
+        // Set the current folder to this filepath
         this.folder = filepath;
 
+        // Read the provided directory, and generate a list of all the items in it
         fs.readdir(filepath, (err, items) => {
-            // Sort the items, ignoring case
-            items.sort(function (a, b) {
-                return a.toLowerCase().localeCompare(b.toLowerCase());
-            });
+            if (!err) {
 
-            // Extract all the supported file extensions and push them
-            // on the 'img_list' array
-            items.forEach((filename, index) => {
-                if (path.extname(filename).match(supported_types)) {
-                    // If the filename matches the current filename, the array length
-                    // is the index of the current file
-                    if (filename === current_filename) {
-                        this.current_index = this.img_list.length;
+                var num_diff;
+
+                // Sort the items, ignoring case
+                items.sort((a, b) => {
+
+                    // Try to sort numerically
+                    if (a.match(/^[0-9]+/) && b.match(/^[0-9]+/)) {
+                        num_diff = a.match(/^[0-9]+/)[0] - b.match(/^[0-9]+/)[0];
+
+                        // If the difference is 0 (numerical part is identical),
+                        // return a unicode diff
+                        if (num_diff === 0) {
+                            return a.toLowerCase().localeCompare(b.toLowerCase());
+                        } else {
+                            return num_diff;
+                        }
+                    }  else {
+                        // Return a unicode diff
+                        return a.toLowerCase().localeCompare(b.toLowerCase());
                     }
-                    this.img_list.push(filename);
-                }
-            });
 
-            // Callback.
-            cb(err);
+                        // return a.match(/^[0-9]+/)[0] - b.match(/^[0-9]+/)[0];
+
+                    // return a.toLowerCase().localeCompare(b.toLowerCase());
+
+                    // else if (a.match(/^[0-9]+/) && !b.match(/^[0-9]+/)) {
+                    //     return -1;
+                    //
+                    // } else if (!a.match(/^[0-9]+/) && b.match(/^[0-9]+/)) {
+                    //     return 1;
+                    //
+                    // }
+                });
+
+                // Extract all the supported file extensions and push them
+                // on the 'img_list' array
+                items.forEach((filename, index) => {
+                    if (path.extname(filename).toLowerCase().match(supported_types)) {
+                        // If the filename matches the current filename, the array length
+                        // is the index of the current file
+                        if (filename === current_filename) {
+                            this.current_index = this.img_list.length;
+                            console.log('Index of current file: ' + this.current_index);
+                        }
+                        this.img_list.push(filename);
+                    }
+                });
+
+                // The image list is finished, so set the 'list_loaded' flag to true
+                list_loaded = true;
+
+                console.log(this.img_list);
+            } else {
+                // If there is an error, callback with it
+                cb(err);
+            }
         });
+    } else {
+        // While finding the image in the array, prevent calls to the previous
+        // or next item.
+        list_loaded = false;
+
+        // Update the current index of the file from the img_list
+        this.img_list.forEach((string, index) => {
+            if (string === current_filename) {
+                this.current_index = index;
+                // console.log('Index of current file: ' + this.current_index);
+                list_loaded = true;
+            }
+        });
+
+        // If the file was not found, there is a fatal error, since it is not
+        // in the array provided.
+        if (!list_loaded) {
+            cb('ERROR: The file "' + current_filename +
+            '" no longer exists in "' + filepath + '"');
+        }
+
+        console.log(this.img_list);
     }
 }
 
@@ -68,45 +139,80 @@ FSmanager.prototype.getCurrent = function() {
 
 // Get the next image filename in the list. If wraparound is true,
 // then at the end of the array return the first image
-FSmanager.prototype.getNext = function(wraparound = false) {
-    if (this.current_index >= this.img_list.length - 1) {
-        if (wraparound) {
-            // Reset the current index to the start of the directory
-            this.current_index = 0;
+FSmanager.prototype.getNext = function(wraparound = false, cb) {
+    if (list_loaded) {
+        // If the current image is the last one
+        if (this.current_index + 1 > this.img_list.length - 1) {
+            if (wraparound) {
+                // Reset the current file to the last item in the directory
+                this.current_index = 0;
+
+                // Callback with the last image, wrapping around to the end of
+                // the directory
+                cb(true, this.img_list[this.current_index]);
+            } else {
+
+                // Callback, informing that the next image isn't ready
+                // (Maybe inform that at end of list)
+                cb(false, null);
+            }
+        } else {
+            // Default behavior
+            // Increment the current file index
+            this.current_index += 1;
+
+            // Callback with the next filename
+            cb(true, this.img_list[this.current_index]);
         }
-
-        // Return the updated index if 'wraparound' is true, or the same
-        // filname if it is false
-        return this.img_list[this.current_index];
+    } else {
+        // The image list hasn't been generated yet, so callback with no value
+        cb(false, null);
     }
-
-    // Increment the current file index
-    this.current_index += 1;
-
-    // Return the new file
-    return this.img_list[this.current_index];
 }
 
 
 // Get the previous image filename in the list. If wraparound is true, then
 // if this has reached the beginning of the array, return the last image
-FSmanager.prototype.getPrev = function(wraparound = false) {
-    if (this.current_index <= 0) {
-        if (wraparound) {
-            // Reset the current file to the last item in the directory
-            this.current_index = this.img_list.length - 1;
+FSmanager.prototype.getPrev = function(wraparound = false, cb) {
+    if (list_loaded) {
+        // The current image is the first one
+        if (this.current_index - 1 < 0) {
+            if (wraparound) {
+                // Reset the current file to the last item in the directory
+                this.current_index = this.img_list.length - 1;
+
+                // Callback with the last image, wrapping around to the end of
+                // the directory
+                cb(true, this.img_list[this.current_index]);
+            } else {
+
+                // Callback, informing that the next image isn't ready
+                // (Maybe inform that at end of list)
+                cb(false, null);
+            }
+        } else {
+            // Default behavior
+            // Decrement the current file index
+            this.current_index -= 1;
+
+            // Callback with the previous filename
+            cb(true, this.img_list[this.current_index]);
         }
-
-        // Return the updated index if 'wraparound' is true, or the same
-        // filname if it is false
-        return this.img_list[this.current_index];
+    } else {
+        // The image list hasn't been generated yet, so callback with no value
+        cb(false, null);
     }
+}
 
-    // Decrement the current file index
-    this.current_index -= 1;
 
-    // Return the new file
-    return this.img_list[this.current_index];
+// Checks that a filename is supported. This does not need the image list
+// to be generated.
+FSmanager.prototype.checkFile = function (filename) {
+    if (path.extname(filename).toLowerCase().match(supported_types)) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 
