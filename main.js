62be266d9858e940df31d9bb14c4a2264756065c
devedge <devedge@outlook.com>
2016-12-27 17:33:40 -0500

    Cleanup messy image resize code

--- main.js
+++ main.js
@@ -14,29 +14,13 @@ const config = new Config();
 let win;
 
 
-
+// Screen dimensions used to scale window
 var screenDimensions = {
     width: 0,
     height: 0,
     x_center: 0,
     y_center: 0
 };
-
-// var sbounds = {
-//     max_width: 0,   // 75% of the current window size
-//     max_height: 0,  // 75% of the current window size
-//     min_width: 0,   // x % of the current window size
-//     min_height: 0   // y % of the current window size
-// };
-
-// Screen dimensions used to scale window
-var screen_dim = [];
-
-// Set the window bounds variables. Should be set elsewhere?
-var bounds = [0, 0, 550, 550];
-
-// Instantiate the genValues variable
-var genValues;
 
 // console.log('Args: ' + process.argv);
 
@@ -50,15 +34,10 @@ app.on('ready', () => {
     const {width, height} = electron.screen.getPrimaryDisplay().workAreaSize;
     screenDimensions.width = width;
     screenDimensions.height = height;
-    
-    // Get the screen size to properly scale the image window
-    // This may be optional depending on user preferences, move to a function?
-    // screen_dim[0] = width;
-    // screen_dim[1] = height;
 
     // Create the browser window
     win = new BrowserWindow({
-        width: 700, // sbounds.min_width for example
+        width: 700,
         height: 700,
         backgroundColor: '#EFF0F1'
         // backgroundColor: '-webkit-linear-gradient(to bottom, #74e3ec, #c7ffe2)'
@@ -73,18 +52,10 @@ app.on('ready', () => {
 
     // Open the DevTools
     // win.webContents.openDevTools()
-    
+
+    // Set screen values in the dimCalc library to automatically resize the
+    // images & window
     dimCalc.setGlobals(screenDimensions);
-
-
-    // Generate the rest of the values after window load to speed loading
-    // times
-    // screen_dim[2] = Math.floor(screen_dim[0] / 2); // screen 'x' center
-    // screen_dim[3] = Math.floor(screen_dim[1] / 2); // screen 'y' center
-
-    // Set maximum screen dimensions to 3/4 of the screen size
-    // bounds[0] = Math.floor((screen_dim[0] / 4) * 3);
-    // bounds[1] = Math.floor((screen_dim[1] / 4) * 3);
 
 
     // Emit when the window is closed
@@ -150,14 +121,9 @@ ipcMain.on('minimize-window', (event) => {
  */
 ipcMain.on('resize-window', (event, dimensions) => {
     // Generate the required dimensions
-    // genValues = genD([dimensions.width, dimensions.height], screen_dim, bounds);
-    
-    // if center option
-    // genValues = dimCalc.centerImage([dimensions.width, dimensions.height], screen_dim, bounds);
-    
     // config.set('animate', false);
     
-    // if resize option
+    // if center option
     var newDimensions = dimCalc.centerImage(dimensions);
     
     console.log('x: ' + newDimensions.x_center + ' y: ' + newDimensions.y_center + ' width: ' + newDimensions.width + ' height: ' + newDimensions.height);
@@ -170,125 +136,12 @@ ipcMain.on('resize-window', (event, dimensions) => {
     }, false);
 
     
-    // else, do nothing?
+    
+    // if resize option
 
-    // Resize window and center it in screen
-    // win.setBounds({
-    //     x: genValues[2],
-    //     y: genValues[3],
-    //     width: genValues[0],
-    //     height: genValues[1]
-    // }, true);
+    
+    // else, do nothing?
     
     // options: keep centered? fancy animate? center until moved?
 });
 
-
-
-// A function to load any external imports after the main window 
-// function loadImports() {
-    // dimCalc = require('./lib/dimCalc');
-    
-    // Call the function that sets global 
-//     dimCalc.setGlobals();
-// }
-
-// post-load function instead?
-
-
-
-
-// todo: clean up console.log() outputs
-// todo: move this massive function to a module
-
-// Generate the required dimensions for the new window size
-// Uses the image size, screen dimensions, and bounds
-// Resizes the longest side, if needed, and scales the other
-/**
-function genD(image_d, screen_d, bnds) {
-    var new_width;
-    var new_height;
-    var scale_width;
-    var scale_height;
-
-    // image_d[0] = image width
-    // image_d[1] = image height
-
-    // screen_d[0] = screen width
-    // screen_d[1] = screen height
-    // screen_d[2] = screen 'x' center
-    // screen_d[3] = screen 'y' center
-
-    // bnds[0] = max_width
-    // bnds[1] = max_height
-    // bnds[2] = min_width
-    // bnds[3] = min_height
-
-
-    // If the width is greater than the max
-    if (image_d[0] > bnds[0]) {
-
-        // If the height is also greater than the max
-        if (image_d[1] > bnds[1]) {
-
-            // Ensure that the scaled side is still less than the bounds
-            scale_width = Math.floor(image_d[0] / (image_d[1] / bnds[1]));
-            scale_height = Math.floor(image_d[1] / (image_d[0] / bnds[0]));
-
-            // The scaled width is larger than the max width bound
-            if (scale_width > bnds[0]) {
-                new_width = bnds[0];
-                new_height = scale_height;
-
-            } else if (scale_height > bnds[1]) {
-                // The scaled height is larger than the max height bound
-                new_width = scale_width;
-                new_height = bnds[1];
-
-            } else {
-                // Neither are larger than their bounds, so pick the width as the
-                // side to maximize
-                new_width = bnds[0];
-                new_height = scale_height;
-            }
-        } else {
-            // Just the width is larger than the max
-            new_width = bnds[0];
-            new_height= Math.floor(image_d[1] / (image_d[0] / bnds[0]));
-        }
-
-    } else if (image_d[1] > bnds[1]) {
-        // Since the first check failed, just the height is larger than the max
-        new_height = bnds[1];
-        new_width = Math.floor(image_d[0] / (image_d[1] / bnds[1]));
-
-    } else {
-        // Neither are larger than the max, so set them back to
-        // their default values
-        new_width = image_d[0];
-        new_height = image_d[1];
-    }
-
-    // If any of the widths are smaller than the min, resize
-    if (new_width < bnds[2]) {
-        new_width = bnds[2];
-    }
-
-    // If any of the heights are smaller than the min, resize
-    if (new_height < bnds[3]) {
-        new_height = bnds[3];
-    }
-
-    // Return the calculated values
-    // new image width
-    // new image height
-    // screen x center (width)
-    // screen y center (height)
-    return [
-        new_width,
-        new_height,
-        Math.floor(screen_d[2] - (new_width / 2)),
-        Math.floor(screen_d[3] - (new_height / 2))
-    ];
-}
-*/
