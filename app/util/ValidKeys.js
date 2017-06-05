ce15b1045bb42958a868f2c5183920c0335db3a7
devedge <devedge@outlook.com>
2017-06-05 14:11:02 -0400

    Refactored the keypress handler with switch case

--- app/util/ValidKeys.js
+++ app/util/ValidKeys.js
@@ -1,10 +1,15 @@
 module.exports = {
-  ZOOM_IN: 38, // the up arrow. this needs to be paired with the shift key
-  ZOOM_OUT: 40, // down arrow, also needs shift key
-  LEFT: 37,
-  UP: 38,
-  RIGHT: 39,
-  DOWN: 40,
-  ESC: 27,
-  SPACE: 32
+  // ZOOM_IN: 38, // the up arrow. this needs to be paired with the shift key
+  // ZOOM_OUT: 40, // down arrow, also needs shift key
+  // LEFT: 37,
+  // UP: 38,
+  // RIGHT: 39,
+  // DOWN: 40,
+  // ESC: 27,
+  // SPACE: 32
+  NEXT: /(right|down|space)/,
+  PREVIOUS: /(left|up)/,
+  ZOOM_IN: /up/,
+  ZOOM_OUT: /down/,
+  MINIMIZE: /esc/
 };
